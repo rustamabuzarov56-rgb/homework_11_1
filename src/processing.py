38619f61778ed58d99856251bee687c7c9175408
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2025-08-28 17:12:54 +0500

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -10,3 +10,8 @@ def filter_by_state(transactions: list, key='EXECUTED') -> list:
             new_list.append(i)
     return new_list
 
+
+def sort_by_date(list_dict: list, sort_order=True) -> list:
+    """  Функция принимает список словарей, параметр задающий порядок сортировки и
+         возвращать новый список, отсортированный по дате  """
+    return sorted(list_dict, key=lambda x: x["date"], reverse=sort_order)
