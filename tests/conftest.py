8911216f5a8a67f978144e745d220b462cafac05
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2025-09-07 14:37:57 +0500

    bug fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,49 +1,66 @@
 import pytest
 
+
 @pytest.fixture
 def card_type():
     return "Visa Platinum 7000792289606361"
+
 
 @pytest.fixture
 def account_number():
     return "Счет 73654108430135874305"
 
+
 @pytest.fixture
 def date():
     return "2024-03-11T02:26:18.671407"
+
 
 @pytest.fixture
 def date_missing():
     return " "
 
+
 @pytest.fixture
 def short_date():
     return "2024-03-11T02"
 
+
 @pytest.fixture
 def list_dict():
-    return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
 
 @pytest.fixture
 def list_dict_2():
-    return ([{'id': 41428829, 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727,  'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}])
+    return [
+        {"id": 41428829, "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "date": "2018-10-14T08:21:33.419441"},
+    ]
+
 
 @pytest.fixture
 def same_dates():
-    return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}])
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 594226727, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
+
 
 @pytest.fixture
 def non_standard_date():
-    return ([{'id': 41428829, 'state': 'EXECUTED', 'date': 'T18:35:29.512364.2019-0-03'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': 'T02:08:58.425572.2018-06-30'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': 'T21:27:25.241689.2018-09-12'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': 'T08:21:33.419441.2018-10-14'}])
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "T18:35:29.512364.2019-0-03"},
+        {"id": 939719570, "state": "EXECUTED", "date": "T02:08:58.425572.2018-06-30"},
+        {"id": 594226727, "state": "CANCELED", "date": "T21:27:25.241689.2018-09-12"},
+        {"id": 615064591, "state": "CANCELED", "date": "T08:21:33.419441.2018-10-14"},
+    ]
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,28 +1,37 @@
 import pytest
-from src.widget import mask_account_card, get_date
+
+from src.widget import get_date, mask_account_card
+
 
 def test_mask_account_card_1(card_type):
     assert mask_account_card(card_type) == "Visa Platinum 7000 79** **** 6361"
 
+
 def test_mask_account_card_2(account_number):
     assert mask_account_card(account_number) == "Счет **4305"
 
-@pytest.mark.parametrize("type, expected_result", [
-    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
-    ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
-    ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
-    ("Счет 64686473678894779589", "Счет **9589"),
-    ("Счет 35383033474447895560", "Счет **5560")
-])
 
+@pytest.mark.parametrize(
+    "type, expected_result",
+    [
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+    ],
+)
 def test_mask_account_card(type, expected_result):
     assert mask_account_card(type) == expected_result
+
 
 def test_get_date(date):
     assert get_date(date) == "11.03.2024"
 
+
 def test_get_short_date(short_date):
     assert get_date(short_date) == "11.03.2024"
+
 
 def test_get_date_missing(date_missing):
     assert get_date(date_missing) == "Дата отсутствует"
