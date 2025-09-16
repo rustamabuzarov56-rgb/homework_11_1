1e943a2d0f841988bbc23d46e7308e7da50490dc
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2025-09-16 21:09:23 +0500

    add tests

--- src/generators.py
+++ src/generators.py
@@ -1,104 +1,23 @@
-from typing import Iterator
+from typing import Any, Iterator
 
-transactions = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
 
-def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
+def filter_by_currency(transactions: list[dict], currency: str = "USD") -> Iterator[Any]:
     """Функция возвращаeт итератор, который поочередно выдает транзакции,
-     где валюта операции соответствует заданной (например, USD)."""
-    for item in transactions:
-        if item["operationAmount"]["currency"]["code"] == currency:
-            yield item
+    где валюта операции соответствует заданной (например, USD)."""
+    for transaction in transactions:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
+            yield transaction
+
 
 def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
-    """ Генератор который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
-    for transact in transactions:
-        yield transact["description"]
+    """Генератор который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+    for transaction in transactions:
+        yield transaction["description"]
 
 
-def card_number_generator(start, end):
-    """ Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
+def card_number_generator(start: int, end: int) -> Iterator[str]:
+    """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
     for i in range(start, end + 1):
         card_number = f"{i:016d}"
-        formatting_card_number = " ".join(card_number[i: i + 4] for i in range(0, 16, 4))
+        formatting_card_number = " ".join(card_number[i:i + 4] for i in range(0, 16, 4))
         yield formatting_card_number
-
-
-
